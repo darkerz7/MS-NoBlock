f9ff531aea111508efa6d2381c3dbd44e4caf0a3
darkerz7 <25752428+darkerz7@users.noreply.github.com>
2025-11-27 00:04:32 +0500

    Update 1.0.1
    Changed to convar

--- src/NoBlock.cs
+++ src/NoBlock.cs
@@ -5,41 +5,75 @@ using Sharp.Shared.GameEntities;
 using Sharp.Shared.HookParams;
 using Sharp.Shared.Listeners;
 using Sharp.Shared.Managers;
+using Sharp.Shared.Objects;
 
 namespace MS_NoBlock
 {
-    public class NoBlock : IModSharpModule, IEntityListener
+    public class NoBlock : IModSharpModule, IEntityListener, IGameListener
     {
         public string DisplayName => "NoBlock";
         public string DisplayAuthor => "DarkerZ[RUS]";
         public NoBlock(ISharedSystem sharedSystem, string dllPath, string sharpPath, Version version, IConfiguration coreConfiguration, bool hotReload)
         {
-            _hooks = sharedSystem.GetHookManager();
+            _modSharp = sharedSystem.GetModSharp();
             _entities = sharedSystem.GetEntityManager();
+            _convars = sharedSystem.GetConVarManager();
         }
-        private readonly IHookManager _hooks;
+        private readonly IModSharp _modSharp;
         private readonly IEntityManager _entities;
+        private readonly IConVarManager _convars;
+
+        private IConVar? g_cvar_teammates;
+        private IConVar? g_cvar_enemies;
+        bool bActivate = false;
 
         public bool Init()
         {
-            _hooks.PlayerSpawnPost.InstallForward(OnPlayerSpawn);
+            g_cvar_teammates = _convars.FindConVar("mp_solid_teammates");
+            if (g_cvar_teammates != null)
+            {
+                g_cvar_teammates.Set(false);
+                _convars.InstallChangeHook(g_cvar_teammates, OnCvarTeammatesChanged);
+            }
+            g_cvar_enemies = _convars.FindConVar("mp_solid_enemies");
+            if (g_cvar_enemies != null)
+            {
+                g_cvar_enemies.Set(false);
+                _convars.InstallChangeHook(g_cvar_enemies, OnCvarEnemiesChanged);
+            }
+            _modSharp.InstallGameListener(this);
             _entities.InstallEntityListener(this);
             return true;
         }
-        public void Shutdown()
+
+        private void OnCvarTeammatesChanged(IConVar conVar)
         {
-            _hooks.PlayerSpawnPost.RemoveForward(OnPlayerSpawn);
-            _entities.RemoveEntityListener(this);
+            if (bActivate && conVar.GetBool()) conVar.Set(false);
         }
 
-        private void OnPlayerSpawn(IPlayerSpawnForwardParams @params)
+        private void OnCvarEnemiesChanged(IConVar conVar)
         {
-            var client = @params.Client;
-            if (client.IsValid && client.GetPlayerController() is { } player && player.GetPawn() is { } pawn && pawn.GetCollisionProperty() is { } property)
-            {
-                if (property.CollisionAttribute.CollisionGroup == CollisionGroupType.Debris) return;
-                pawn.SetCollisionGroup(CollisionGroupType.Debris);
-            }
+            if (bActivate && conVar.GetBool()) conVar.Set(false);
+        }
+
+        public void OnGameActivate()
+        {
+            bActivate = true;
+            g_cvar_teammates?.Set(false);
+            g_cvar_enemies?.Set(false);
+        }
+
+        public void OnGameDeactivate()
+        {
+            bActivate = false;
+        }
+
+        public void Shutdown()
+        {
+            if (g_cvar_teammates != null) _convars.RemoveChangeHook(g_cvar_teammates, OnCvarTeammatesChanged);
+            if (g_cvar_enemies != null) _convars.RemoveChangeHook(g_cvar_enemies, OnCvarEnemiesChanged);
+            _modSharp!.RemoveGameListener(this);
+            _entities.RemoveEntityListener(this);
         }
 
         public void OnEntitySpawned(IBaseEntity entity)
@@ -50,6 +84,8 @@ namespace MS_NoBlock
             }
         }
 
+        int IGameListener.ListenerVersion => IGameListener.ApiVersion;
+        int IGameListener.ListenerPriority => 0;
         int IEntityListener.ListenerVersion => IEntityListener.ApiVersion;
         int IEntityListener.ListenerPriority => 0;
     }
