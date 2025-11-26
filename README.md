0c2bdfe2eaa315262213355808d3e2bed4d6b473
darkerz7 <25752428+darkerz7@users.noreply.github.com>
2025-11-26 16:41:13 +0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# MS-NoBlock
+# MS-NoBlock for ModSharp
 Allows players to pass through each other without collision
+
+## Required packages:
+1. [ModSharp](https://github.com/Kxnrl/modsharp-public)
+
+## Installation:
+1. Compile or copy MS-NoBlock to `sharp/modules/MS-NoBlock` folger
---
+++ src/NoBlock.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using Sharp.Shared;
+using Sharp.Shared.Enums;
+using Sharp.Shared.GameEntities;
+using Sharp.Shared.HookParams;
+using Sharp.Shared.Listeners;
+using Sharp.Shared.Managers;
+
+namespace MS_NoBlock
+{
+    public class NoBlock : IModSharpModule, IEntityListener
+    {
+        public string DisplayName => "NoBlock";
+        public string DisplayAuthor => "DarkerZ[RUS]";
+        public NoBlock(ISharedSystem sharedSystem, string dllPath, string sharpPath, Version version, IConfiguration coreConfiguration, bool hotReload)
+        {
+            _hooks = sharedSystem.GetHookManager();
+            _entities = sharedSystem.GetEntityManager();
+        }
+        private readonly IHookManager _hooks;
+        private readonly IEntityManager _entities;
+
+        public bool Init()
+        {
+            _hooks.PlayerSpawnPost.InstallForward(OnPlayerSpawn);
+            _entities.InstallEntityListener(this);
+            return true;
+        }
+        public void Shutdown()
+        {
+            _hooks.PlayerSpawnPost.RemoveForward(OnPlayerSpawn);
+            _entities.RemoveEntityListener(this);
+        }
+
+        private void OnPlayerSpawn(IPlayerSpawnForwardParams @params)
+        {
+            var client = @params.Client;
+            if (client.IsValid && client.GetPlayerController() is { } player && player.GetPawn() is { } pawn && pawn.GetCollisionProperty() is { } property)
+            {
+                if (property.CollisionAttribute.CollisionGroup == CollisionGroupType.Debris) return;
+                pawn.SetCollisionGroup(CollisionGroupType.Debris);
+            }
+        }
+
+        public void OnEntitySpawned(IBaseEntity entity)
+        {
+            if (entity.Classname.EndsWith("_projectile", StringComparison.OrdinalIgnoreCase))
+            {
+                entity.SetCollisionGroup(CollisionGroupType.Debris);
+            }
+        }
+
+        int IEntityListener.ListenerVersion => IEntityListener.ApiVersion;
+        int IEntityListener.ListenerPriority => 0;
+    }
+}
